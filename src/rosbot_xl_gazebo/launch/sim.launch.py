de48135af9145fc3cca168f6d4a07e56ac197b48
Kotochleb <krzy.wojciecho@gmail.com>
2022-05-12 13:07:00 +0200

    Add nav2 to simulation

--- src/rosbot_xl_gazebo/launch/sim.launch.py
+++ src/rosbot_xl_gazebo/launch/sim.launch.py
@@ -10,13 +10,13 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
   rosbot_xl_description = get_package_share_directory('rosbot_xl_description')
   rosbot_xl_gazebo = get_package_share_directory('rosbot_xl_gazebo')
-  # world = LaunchConfiguration('world', default=os.path.join(rosbot_xl_gazebo, 'worlds', ' turtlebot_playground.world'))
+  world = LaunchConfiguration('world', default=os.path.join(rosbot_xl_gazebo, 'worlds', 'turtlebot_playground.world'))
 
   return LaunchDescription([
     IncludeLaunchDescription(
       PythonLaunchDescriptionSource([rosbot_xl_gazebo, '/launch/world.launch.py']),
       launch_arguments = {
-        'world' : '',
+        'world' : world,
       }.items(),
     ),
 

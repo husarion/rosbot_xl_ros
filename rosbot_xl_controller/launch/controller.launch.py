4e40d40859852fc0d3df4074d77267bac76679c3
Maciej Stepien <maciej.stepien@husarion.com>
2023-02-22 16:47:37 +0000

    Add camera, lidar and camera mount arguments to launch files

--- rosbot_xl_controller/launch/controller.launch.py
+++ rosbot_xl_controller/launch/controller.launch.py
@@ -46,6 +46,27 @@ def generate_launch_description():
         description="Whether simulation is used",
     )
 
+    lidar_model = LaunchConfiguration("lidar_model")
+    declare_lidar_model_arg = DeclareLaunchArgument(
+        "lidar_model",
+        default_value="slamtec_rplidar_s1",
+        description="Lidar model added to the URDF",
+    )
+
+    camera_model = LaunchConfiguration("camera_model")
+    declare_camera_model_arg = DeclareLaunchArgument(
+        "camera_model",
+        default_value="None",
+        description="Camera model added to the URDF",
+    )
+
+    include_camera_mount = LaunchConfiguration("include_camera_mount")
+    declare_include_camera_mount_arg = DeclareLaunchArgument(
+        "include_camera_mount",
+        default_value="False",
+        description="Whether to include camera mount to the robot URDF",
+    )
+
     controller_config_name = PythonExpression(
         [
             "'mecanum_drive_controller.yaml' if ",
@@ -78,9 +99,12 @@ def generate_launch_description():
             mecanum,
             " use_sim:=",
             use_sim,
-            " lidar_model:=None",
-            " camera_model:=None",
-            " include_camera_mount:=true",
+            " lidar_model:=",
+            lidar_model,
+            " camera_model:=",
+            camera_model,
+            " include_camera_mount:=",
+            include_camera_mount,
         ]
     )
     robot_description = {"robot_description": robot_description_content}
@@ -171,6 +195,9 @@ def generate_launch_description():
     actions = [
         declare_mecanum_arg,
         declare_use_sim_arg,
+        declare_lidar_model_arg,
+        declare_camera_model_arg,
+        declare_include_camera_mount_arg,
         SetParameter(name="use_sim_time", value=use_sim),
         control_node,
         robot_state_pub_node,

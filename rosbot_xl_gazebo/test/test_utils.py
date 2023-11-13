e571d9afc556fa6de26c901a65ef23bcf180a427
rafal-gorecki <rafal.gorecki@husarion.com>
2023-11-13 14:26:17 +0100

    Change test velocity accuracy limits

--- rosbot_xl_gazebo/test/test_utils.py
+++ rosbot_xl_gazebo/test/test_utils.py
@@ -34,7 +34,7 @@ class SimulationTestNode(Node):
     __test__ = False
     # The inaccuracies in measurement uncertainties and wheel slippage
     # cause the rosbot_xl_base_controller to determine inaccurate odometry.
-    ACCURACY = 0.20  # 20% accuracy
+    ACCURACY = 0.10  # 10% accuracy
 
     def __init__(self, name="test_node"):
         super().__init__(name)

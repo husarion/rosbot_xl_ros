7dbb6c10cce76c67028185b2c9935002a3c3ad98
Jakub Delicat <jakub.delicat@husarion.com>
2024-01-16 11:38:23 +0100

    Createad package utils | added flash script
    Signed-off-by: Jakub Delicat <jakub.delicat@husarion.com>

---
+++ rosbot_xl_utils/rosbot_xl_utils/flash-firmware.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+
+# Copyright 2023 Husarion
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sh
+import time
+import sys
+import argparse
+from pyftdi.ftdi import Ftdi
+
+# CBUS0 - BOOT0
+# CBUS1 - RST
+
+
+class FirmwareFlasher:
+    def __init__(self, binary_file, port):
+        # ftdi.show_devices()
+        # self.ftdi = Ftdi.create_from_url('ftdi://ftdi:ft-x:DK0AM0V0/1')
+        self.device = "ftdi://ftdi:ft-x:/1"
+        self.ftdi = Ftdi()
+
+        self.binary_file = binary_file
+        self.max_approach_no = 5
+        self.port = port
+
+    def enter_bootloader_mode(self):
+        self.ftdi.open_from_url(url=self.device)
+        self.ftdi.set_cbus_direction(0b11, 0b11)  # set CBUS0 and CBUS1 to output
+        time.sleep(0.1)
+        self.ftdi.set_cbus_gpio(0b11)  # set CBUS0 to 1 and RST to 1
+        time.sleep(0.1)
+        self.ftdi.set_cbus_gpio(0b01)  # set CBUS0 to 1 and RST to 0
+        time.sleep(0.1)
+        # self.ftdi.set_cbus_direction(0b11,0b00) # set CBUS0 and CBUS1 to input
+        time.sleep(0.1)
+        self.ftdi.close()
+
+    def exit_bootloader_mode(self):
+        self.ftdi.open_from_url(url=self.device)
+        self.ftdi.set_cbus_direction(0b11, 0b11)  # set CBUS0 and CBUS1 to output
+        time.sleep(0.1)
+        self.ftdi.set_cbus_gpio(0b10)  # set CBUS0 to 1 and RST to 1
+        time.sleep(0.1)
+        self.ftdi.set_cbus_gpio(0b00)  # set CBUS0 to 1 and RST to 0
+        time.sleep(0.1)
+        # self.ftdi.set_cbus_direction(0b11,0b00) # set CBUS0 and CBUS1 to input
+        time.sleep(0.1)
+        self.ftdi.close()
+
+    def flash_firmware(self):
+        self.enter_bootloader_mode()
+        sh.usbreset(
+            "0403:6015"
+        )  # workaround: using pyftdi causes laggy serial port. This line is like unplug/plug for USB port
+        sh.stm32flash(self.port, "-v", w=self.binary_file, b="115200", _out=sys.stdout)
+        self.exit_bootloader_mode()
+        sh.usbreset("0403:6015")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Flashing the firmware on STM32 microcontroller in ROSbot XL"
+    )
+
+    parser.add_argument(
+        "-f",
+        "--file",
+        nargs="?",
+        default="/firmware.bin",
+        help="Path to a firmware file. Default: /firmware.bin",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        nargs="?",
+        default="/dev/ttyUSB0",
+        help="Path to serial connection. Default: /dev/ttyUSB0",
+    )
+
+    binary_file = parser.parse_args().file
+    port = parser.parse_args().port
+
+    flasher = FirmwareFlasher(binary_file, port)
+    flasher.flash_firmware()
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ rosbot_xl_utils/setup.py
@@ -0,0 +1,37 @@
+# Copyright 2023 Husarion
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import find_packages, setup
+
+package_name = "rosbot_xl_utils"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Husarion",
+    maintainer_email="support@husarion.com",
+    description="Utilities for ROSbot XL",
+    license="Apache License 2.0",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": ["flash_firmware = rosbot_xl_utils.flash_firmware:main"],
+    },
+)

fcf454a0586d4b580105bd32a9c0b84fdc4c3315
Jakub Delicat <jakub.delicat@husarion.com>
2024-01-25 09:20:58 +0100

    Added bringup and controller tests
    Signed-off-by: Jakub Delicat <jakub.delicat@husarion.com>

---
+++ rosbot_xl_bringup/test/test_utils.py
@@ -0,0 +1,123 @@
+# Copyright 2021 Open Source Robotics Foundation, Inc.
+# Copyright 2023 Husarion
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+import math
+import random
+
+from threading import Event
+from threading import Thread
+
+from rclpy.node import Node
+
+from sensor_msgs.msg import JointState, Imu, LaserScan
+from tf2_ros import TransformException
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
+
+
+class BringupTestNode(Node):
+    ROSBOT_HARDWARE_PUBLISHERS_RATE = 10.0
+    __test__ = False
+
+    def __init__(self, name="test_node", namespace=None):
+        super().__init__(
+            name,
+            namespace=namespace,
+            cli_args=["--ros-args", "-r", "/tf:=tf", "-r", "/tf_static:=tf_static"],
+        )
+        self.odom_tf_event = Event()
+        self.scan_filter_event = Event()
+
+    def create_test_subscribers_and_publishers(self):
+        self.imu_publisher = self.create_publisher(Imu, "/_imu/data_raw", 10)
+
+        self.joint_states_publisher = self.create_publisher(
+            JointState, "/_motors_response", 10
+        )
+
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+
+        self.scan_publisher = self.create_publisher(LaserScan, "scan", 10)
+        self.filtered_scan_subscriber = self.create_subscription(
+            LaserScan, "scan_filtered", self.filtered_scan_callback, 10
+        )
+
+        self.timer = None
+
+    def lookup_transform_odom(self):
+        try:
+            self.tf_buffer.lookup_transform("odom", "base_link", rclpy.time.Time())
+            self.odom_tf_event.set()
+        except TransformException as ex:
+            self.get_logger().error(f"Could not transform odom to base_link: {ex}")
+
+    def start_node_thread(self):
+        self.ros_spin_thread = Thread(
+            target=lambda node: rclpy.spin(node), args=(self,)
+        )
+        self.ros_spin_thread.start()
+
+    def start_publishing_fake_hardware(self):
+        self.timer = self.create_timer(
+            1.0 / self.ROSBOT_HARDWARE_PUBLISHERS_RATE,
+            self.timer_callback,
+        )
+
+    def filtered_scan_callback(self, msg: LaserScan):
+        if len(msg.ranges) > 0:
+            self.scan_filter_event.set()
+
+    def timer_callback(self):
+        self.publish_fake_hardware_messages()
+        self.lookup_transform_odom()
+        self.publish_scan()
+
+    def publish_fake_hardware_messages(self):
+        imu_msg = Imu()
+        imu_msg.header.stamp = self.get_clock().now().to_msg()
+        imu_msg.header.frame_id = "imu_link"
+
+        joint_state_msg = JointState()
+        joint_state_msg.header.stamp = self.get_clock().now().to_msg()
+        joint_state_msg.name = [
+            "fl_wheel_joint",
+            "fr_wheel_joint",
+            "rl_wheel_joint",
+            "rr_wheel_joint",
+        ]
+        joint_state_msg.position = [0.0, 0.0, 0.0, 0.0]
+        joint_state_msg.velocity = [0.0, 0.0, 0.0, 0.0]
+
+        self.imu_publisher.publish(imu_msg)
+        self.joint_states_publisher.publish(joint_state_msg)
+
+    def publish_scan(self):
+        msg = LaserScan()
+        msg.header.frame_id = "laser"
+        msg.angle_min = 0.0
+        msg.angle_max = 2.0 * math.pi
+        msg.angle_increment = 0.05
+        msg.time_increment = 0.1
+        msg.scan_time = 0.1
+        msg.range_min = 0.0
+        msg.range_max = 10.0
+
+        # fill ranges from 0.0m to 1.0m
+        msg.ranges = [
+            random.random() for _ in range(int(msg.angle_max / msg.angle_increment))
+        ]
+        self.scan_publisher.publish(msg)
---
+++ rosbot_xl_controller/test/test_mecanum_controllers.py
@@ -0,0 +1,106 @@
+# Copyright 2021 Open Source Robotics Foundation, Inc.
+# Copyright 2023 Intel Corporation. All Rights Reserved.
+# Copyright 2023 Husarion
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import launch_pytest
+import pytest
+import rclpy
+import os
+import random
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from test_utils import ControllersTestNode
+
+
+@launch_pytest.fixture
+def generate_test_description():
+    proc_env = os.environ.copy()
+    proc_env["ROS_LOCALHOST_ONLY"] = "1"
+    proc_env["ROS_DOMAIN_ID"] = random.randint(0, 255)
+
+    rosbot_xl_controller = get_package_share_directory("rosbot_xl_controller")
+    bringup_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [
+                    rosbot_xl_controller,
+                    "launch",
+                    "controller.launch.py",
+                ]
+            )
+        ),
+        launch_arguments={
+            "use_sim": "False",
+            "mecanum": "True",
+        }.items(),
+    )
+
+    return LaunchDescription([bringup_launch])
+
+
+@pytest.mark.launch(fixture=generate_test_description)
+def test_controllers_startup_fail():
+    rclpy.init()
+    try:
+        node = ControllersTestNode("test_controllers_startup_fail")
+        node.create_test_subscribers_and_publishers()
+
+        node.start_node_thread()
+        msgs_received_flag = node.joint_state_msg_event.wait(timeout=10.0)
+        assert not msgs_received_flag, (
+            "Received JointStates message that should not have appeared. Check whether other"
+            " robots are connected to your network.!"
+        )
+        msgs_received_flag = node.odom_msg_event.wait(timeout=10.0)
+        assert not msgs_received_flag, (
+            "Received Odom message that should not have appeared. Check whether other robots are"
+            " connected to your network.!"
+        )
+        msgs_received_flag = node.imu_msg_event.wait(timeout=10.0)
+        assert not msgs_received_flag, (
+            "Received Imu message that should not have appeared. Check whether other robots are"
+            " connected to your network.!"
+        )
+    finally:
+        rclpy.shutdown()
+
+
+@pytest.mark.launch(fixture=generate_test_description)
+def test_controllers_startup_success():
+    rclpy.init()
+    try:
+        node = ControllersTestNode("test_controllers_startup_success")
+        node.create_test_subscribers_and_publishers()
+        node.start_publishing_fake_hardware()
+
+        node.start_node_thread()
+        msgs_received_flag = node.joint_state_msg_event.wait(timeout=10.0)
+        assert (
+            msgs_received_flag
+        ), "Expected JointStates message but it was not received. Check joint_state_broadcaster!"
+        msgs_received_flag = node.odom_msg_event.wait(timeout=10.0)
+        assert (
+            msgs_received_flag
+        ), "Expected Odom message but it was not received. Check rosbot_xl_base_controller!"
+        msgs_received_flag = node.imu_msg_event.wait(timeout=10.0)
+        assert (
+            msgs_received_flag
+        ), "Expected Imu message but it was not received. Check imu_broadcaster!"
+    finally:
+        rclpy.shutdown()

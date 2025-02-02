7fa8175ed959cce96339d20335ba97778381b587
rafal.gorecki <rafal.gorecki@husarion.com>
2025-02-02 14:21:42 +0100

    Add components

--- rosbot_xl_description/launch/load_urdf.launch.py
+++ rosbot_xl_description/launch/load_urdf.launch.py
@@ -14,27 +14,88 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import yaml
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.conditions import IfCondition
 from launch.substitutions import (
     Command,
     FindExecutable,
     LaunchConfiguration,
     PathJoinSubstitution,
-    PythonExpression,
 )
-from launch_ros.actions import Node, SetParameter
+from launch_ros.actions import Node, SetParameter, SetRemap
 from launch_ros.substitutions import FindPackageShare
 
 
+def contains_cam_component(yaml_fil):
+    with open(yaml_fil, "r") as file:
+        data = yaml.safe_load(file)
+        if "components" in data:
+            return any(item["type"].startswith("CAM") for item in data["components"])
+    return False
+
+def launch_setup(context, *args, **kwargs):
+    components_config = LaunchConfiguration("components_config").perform(context)
+    mecanum = LaunchConfiguration("mecanum").perform(context)
+    robot_model = LaunchConfiguration("robot_model").perform(context)
+    use_joint_state_publisher = LaunchConfiguration(
+        "use_joint_state_publisher", default="True"
+    ).perform(context)
+    use_sim = LaunchConfiguration("use_sim").perform(context)
+
+    controller_config = (
+        "mecanum_drive_controller.yaml" if eval(mecanum) else "diff_drive_controller.yaml"
+    )
+    controller_config = PathJoinSubstitution(
+        [FindPackageShare("rosbot_xl_controller"), "config", controller_config]
+    )
+
+    urdf_file = robot_model + ".urdf.xacro"
+    include_camera_mount = str(contains_cam_component(components_config))
+    print("include_camera_mount: ", include_camera_mount)
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution([FindPackageShare("rosbot_xl_description"), "urdf", urdf_file]),
+            " components_config:=",
+            components_config,
+            " controller_config:=",
+            controller_config,
+            " include_camera_mount:=",
+            include_camera_mount,
+            " mecanum:=",
+            mecanum,
+            " use_sim:=",
+            use_sim,
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        condition=IfCondition(use_joint_state_publisher),
+    )
+
+    return [
+        SetParameter(name="use_sim_time", value=use_sim),
+        SetRemap("/tf", "tf"),
+        SetRemap("/tf_static", "tf_static"),
+        robot_state_pub_node,
+        joint_state_publisher_node,
+    ]
+
+
 def generate_launch_description():
-    components_config = LaunchConfiguration("components_config")
-    mecanum = LaunchConfiguration("mecanum")
-    robot_model = LaunchConfiguration("robot_model")
-    use_joint_state_publisher = LaunchConfiguration("use_joint_state_publisher", default="True")
-    use_sim = LaunchConfiguration("use_sim")
-    
     declare_components_config_arg = DeclareLaunchArgument(
         "components_config",
         default_value=PathJoinSubstitution(
@@ -66,54 +127,7 @@ def generate_launch_description():
         choices=["True", "False"],
     )
 
-    controller_config = PythonExpression(
-        [
-            "'mecanum_drive_controller.yaml' if ",
-            mecanum,
-            " else 'diff_drive_controller.yaml'",
-        ]
-    )
-
-    controller_config = PathJoinSubstitution(
-        [
-            FindPackageShare("rosbot_xl_controller"),
-            "config",
-            controller_config,
-        ]
-    )
-
-    urdf_file = PythonExpression(["'", robot_model, ".urdf.xacro'"])
-    robot_description_content = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
-            PathJoinSubstitution([FindPackageShare("rosbot_xl_description"), "urdf", urdf_file]),
-            " components_config:=",
-            components_config,
-            " controller_config:=",
-            controller_config,
-            " mecanum:=",
-            mecanum,
-            " use_sim:=",
-            use_sim,
-        ]
-    )
-    robot_description = {"robot_description": robot_description_content}
-
-    robot_state_pub_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        output="both",
-        parameters=[robot_description],
-        remappings=[("/tf", "tf"), ("/tf_static", "tf_static")],
-    )
-
-    joint_state_publisher_node = Node(
-        package="joint_state_publisher",
-        executable="joint_state_publisher",
-        emulate_tty=True,
-        condition=IfCondition(use_joint_state_publisher),
-    )
+    publish_robot_description = OpaqueFunction(function=launch_setup)
 
     return LaunchDescription(
         [
@@ -121,8 +135,6 @@ def generate_launch_description():
             declare_mecanum_arg,
             declare_robot_model_arg,
             declare_use_sim_arg,
-            SetParameter(name="use_sim_time", value=use_sim),
-            robot_state_pub_node,
-            joint_state_publisher_node,
+            publish_robot_description,
         ]
     )
